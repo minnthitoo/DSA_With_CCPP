03a21fc99de40a10ff4ac743d01f66157ef3be84
minnthitoo <mto.forlearning@gmail.com>
2022-05-25 22:25:12 +0630

    char array day

---
+++ char_array.c
@@ -0,0 +1,28 @@
+//
+// Created by minnthitoo on ၂၅-၀၅-၂၂.
+//
+#include <stdio.h>
+#define SIZE 30
+
+int main(){
+//    char arr[10] = "I am a mto";
+//    printf("%s", arr);
+
+//    char name[SIZE];
+//    printf("Please enter your name:");
+//    scanf("%s", name); //terminate when meet whitespace
+//    printf("%s", name);
+
+//    char next_n[SIZE];
+//    printf("Please enter your name:");
+//    scanf("%[^\n]", next_n);
+//    printf("%s\n", next_n);
+
+    char anoterh_n[SIZE];
+    printf("Please enter your name:");
+    gets(anoterh_n);
+    printf("%s", anoterh_n);
+
+    return 0;
+}
+
