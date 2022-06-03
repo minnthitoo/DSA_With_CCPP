9c2f170c63d2ae5a9a54a9393507e4b63e59caf6
minnthitoo <mto.forlearning@gmail.com>
2022-06-03 18:44:08 +0630

    char array

--- pointerChar.c
+++ pointerChar.c
@@ -1,4 +1,20 @@
 //
 // Created by minnthitoo on ၀၁-၀၆-၂၂.
 //
+#include <stdio.h>
+
+int main(){
+    char *charPtr;
+    charPtr = "Minn thit Oo";
+
+    printf("My name is %s", charPtr);
+    printf("\nThe size of charPtr is %lu", sizeof(charPtr));
+    /*
+     * The size of char pointer on 32 bit processor => 4 bytes
+     * The size of char pointer on 64 bit processor => 8 bytes
+     */
+
+
+    return 0;
+}
 
